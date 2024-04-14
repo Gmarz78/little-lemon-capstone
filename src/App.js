eb01b73540d7694879fc71b8fcd65142487db5ab
Gordon Marshall <Gmarz78@gmail.com>
2024-04-14 13:04:13 +0100

    Added semantic HTML structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import "./App.css";
 function App() {
     return (
         <>
-            <header></header>
+            <header>
+                <nav>
+                    <h1>Little Lemon</h1>
+                </nav>
+            </header>
+            <main>
+                <section>
+                    <h2>Hero</h2>
+                </section>
+                <section>
+                    <h2>Highlights</h2>
+                </section>
+                <section>
+                    <h2>Testimonials</h2>
+                </section>
+                <section>
+                    <h2>About</h2>
+                </section>
+            </main>
+            <footer>
+                <h2>Footer</h2>
+            </footer>
         </>
     );
 }
